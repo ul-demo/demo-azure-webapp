9a8ddf5fdd505aa1318f4bc83462d65530bbe1d0
frpol9 <frederic.poliquin@dti.ulaval.ca>
2020-11-30 10:13:39 -0500

    Ajout d'un controlleur qui retoune "Allo"

---
+++ src/main/java/ca/ulaval/demo/demoazurewebapp/DemoController.java
@@ -0,0 +1,13 @@
+package ca.ulaval.demo.demoazurewebapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String allo() {
+        return "Allo";
+    }
+}
