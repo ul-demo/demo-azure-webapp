c085b349da573722153f9f496d027fc4897feabc
frpol9 <frederic.poliquin@dti.ulaval.ca>
2020-11-30 09:37:46 -0500

    Ajout du starter de Spring Boot

---
+++ src/main/java/ca/ulaval/demo/demoazurewebapp/DemoAzureWebappApplication.java
@@ -0,0 +1,13 @@
+package ca.ulaval.demo.demoazurewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAzureWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAzureWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/ulaval/demo/demoazurewebapp/DemoAzureWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.ulaval.demo.demoazurewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAzureWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
